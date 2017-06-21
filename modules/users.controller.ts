60558de42af528fb94b380307e7f9c32ed0726f1
douglasbolis <douglas.lima@xdevel.com.br>
2017-06-21 16:40:51 -0300

    feat(controllers): Rewrite UsersController adding async/await to methods.

--- modules/users.controller.ts
+++ modules/users.controller.ts
@@ -6,20 +6,20 @@ import {
 export class UsersController {
 	constructor( private usersService: UsersController ) {}
 	@Get()
-	public getAllUsers( @Response res ) {
-		return this.usersService.getAllUsers()
-			.then( users => res.status( HttpStatus.OK ).json( users ) )
+	public async getAllUsers( @Response res ) {
+		const users = await this.usersService.getAllUsers()
+		return res.status( HttpStatus.OK ).json( users )
 	}
 
 	@Get( '/:id' )
-	public getUser( @Response() res, @Param( 'id' ) id ) {
-		return this.usersService.getUser( +id )
-			.then( user => res.status( HttpStatus.OK ).json( user ) )
+	public async getUser( @Response() res, @Param( 'id' ) id ) {
+		const user = await this.usersService.getUser( +id )
+		return res.status( HttpStatus.OK ).json( user )
 	}
 
 	@Post()
-	public addUser( @Response res, @Body( 'users' ) user ) {
-		return this.usersService.addUser( user )
-			.then( message => res.status( HttpStatus.CREATED ).json( message ) )
+	public async addUser( @Response res, @Body( 'users' ) user ) {
+		const message = await this.usersService.addUser( user )
+		return res.status( HttpStatus.CREATED ).json( message )
 	}
 }
